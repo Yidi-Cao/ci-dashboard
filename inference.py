505f050a09ea13a8be6379349679119a12d5e447
Shaojie li <shaojie.li@bcgdv.com>
2023-10-17 14:14:42 +0800

    rm open ai key

--- inference.py
+++ inference.py
@@ -62,6 +62,5 @@ def init_gpt_4():
     openai.api_type = "azure"
     openai.api_version = "2023-07-01-preview"
     openai.api_key = "90c9e8bcffc040fc9e2ddf0479309f7b"
-    # openai.api_key = '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'
 
     return openai, engine
